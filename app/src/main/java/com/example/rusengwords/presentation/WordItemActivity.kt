9f280bc9de2bec78fb961bbeae2c01a271daf992
Hrapus <hrapus@gmail.com>
2023-08-26 17:19:17 -0400

    Word Screen ViewModel

---
+++ app/src/main/java/com/example/rusengwords/presentation/WordItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rusengwords.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rusengwords.R
+
+class WordItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_item)
+    }
+}
---
+++ app/src/main/java/com/example/rusengwords/presentation/WordItemViewModel.kt
@@ -0,0 +1,80 @@
+package com.example.rusengwords.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.rusengwords.data.WordListRepositoryImpl
+import com.example.rusengwords.domain.AddWordUseCase
+import com.example.rusengwords.domain.EditWordUseCase
+import com.example.rusengwords.domain.GetWordByIdUseCase
+import com.example.rusengwords.domain.WordUnit
+
+class WordItemViewModel: ViewModel() {
+
+    private val repository = WordListRepositoryImpl
+
+    private val getWordUseCase = GetWordByIdUseCase(repository)
+    private val addWordUseCase = AddWordUseCase(repository)
+    private val editWordUseCase = EditWordUseCase(repository)
+
+    private val _errorInputWord = MutableLiveData<Boolean>()
+    val errorInputWord: LiveData<Boolean>
+        get() = _errorInputWord
+
+    private val _word = MutableLiveData<WordUnit>()
+    val word: LiveData<WordUnit>
+        get() = _word
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen : LiveData<Unit>
+        get() = _shouldCloseScreen
+
+    fun getWord(wordId: Int){
+        val word= getWordUseCase.getWord(wordId)
+        _word.value = word
+    }
+
+    fun addWord(eng: String?, rus: String?){
+        val engWord = parseWord(eng)
+        val rusWord = parseWord(rus)
+        val fieldsValid = validateInput(engWord, rusWord)
+        if (fieldsValid) {
+            addWordUseCase.addWord(WordUnit(rusWord, engWord, true))
+        }
+        finishWork()
+    }
+
+    fun editWord(eng: String?, rus: String?){
+        val engWord = parseWord(eng)
+        val rusWord = parseWord(rus)
+        val fieldsValid = validateInput(engWord, rusWord)
+        if (fieldsValid) {
+            _word.value?.let {
+                val word = it.copy(rusWord = rusWord, engWord = engWord)
+                editWordUseCase.editWord(word)
+                finishWork()
+            }
+        }
+    }
+
+    private fun parseWord(input: String?): String{
+        return input?.trim() ?: ""
+    }
+
+    private fun validateInput(eng: String, rus: String): Boolean {
+        var result = true
+        if (eng.isBlank() || rus.isBlank()){
+            _errorInputWord.value = false
+            result = false
+        }
+        return result
+    }
+
+    public fun resetError(){
+        _errorInputWord.value = true
+    }
+
+    private fun finishWork(){
+        _shouldCloseScreen.value = Unit
+    }
+}
